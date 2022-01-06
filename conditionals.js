229b6ec07cb8338f751acb4b6f9c5451be3f03ea
Daryan Zycki <da.zycki@gmail.com>
2022-01-06 03:15:26 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,6 +1,7 @@
+//IF STATEMENTS
+
 //first example 
 
-/*
 const raining = true;
 const cold = false;
 
@@ -13,11 +14,11 @@ if (cold) {
 }
 
 console.log("Now you're ready to go outside!");
-*/
+
 
 //second example 
 
-/*
+
 const cold = false;
 
 if (cold) {
@@ -25,7 +26,7 @@ if (cold) {
 } else {
   console.log("Short sleeves are fine.");
 }
-*/
+
 
 //third example
 
@@ -40,3 +41,27 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+//LOGICAL OPERATORS
+
+//first example
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+
+//second example 
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+//third example
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
