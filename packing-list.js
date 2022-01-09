9fb84c9d6790ee168fd15636e5265918ea7ec98c
Daryan Zycki <da.zycki@gmail.com>
2022-01-09 21:48:12 +0000

    Added a function to loop over items in an array using a FOR and WHILE loop.

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,21 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+//using FOR loop
+
+/*
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 } //prints all the items in the list 1 by 1
 
 packingList[3]; // prints pans
+*/
+
+//using WHILE loop
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
