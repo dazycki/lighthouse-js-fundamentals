7eafd902525e8984e392ba47640be7a9e0f57ebf
Daryan Zycki <da.zycki@gmail.com>
2022-01-09 15:56:34 +0000

    Added a function to determine if a number is even.

---
+++ even.js
@@ -0,0 +1,17 @@
+// isEven function
+
+const isEven = function (num) {
+  return num % 2 === 0;
+} //declaring the function
+
+/*
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven); //returns true
+console.log(elevenIsEven); //returns false
+*/
+
+//simpplified version of above code
+console.log(isEven(10)); //returns true
+console.log(isEven(11)); //returns false
