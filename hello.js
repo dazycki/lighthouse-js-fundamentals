3701f0e66eb8f57a2909d8016d0d3ca4bc2d6c0a
Daryan Zycki <da.zycki@gmail.com>
2022-01-09 15:50:55 +0000

    Added an example to distinguish return vs console.log

--- hello.js
+++ hello.js
@@ -17,3 +17,24 @@ const sayHello = function (name) {
 sayHello("Caliban"); //prints Hello, Caliban
 sayHello("Miranda"); //prints Hello, Miranda
 sayHello("Ferdinand"); //prints Hello, Ferdinand
+
+// /return & console.log
+
+//example 1 
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); //prints Hello, John to console
+
+//example 2
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John'); // does not print anything
+
+//example 3
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting); //prints Hello, John to console
