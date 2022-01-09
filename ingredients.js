074751ef8573ae0bb38c07451af1a1607e58927d
Daryan Zycki <da.zycki@gmail.com>
2022-01-09 21:58:00 +0000

    Loop through an array using FOR and WHILE loop. Also reverse the list.

---
+++ ingredients.js
@@ -0,0 +1,22 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+console.log("Ingredients needed are:");
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+// Write a for loop that prints out the contents of ingredients:
+console.log("Ingredients needed are:");
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+console.log("Ingredients in reverse order are:");
+for (let i = ingredients.length - 1; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
+
