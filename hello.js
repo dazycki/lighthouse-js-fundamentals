e3057aad673bac9b8118b6664b871c86615ade02
Daryan Zycki <da.zycki@gmail.com>
2022-01-09 15:47:47 +0000

    Use a function to print names

---
+++ hello.js
@@ -0,0 +1,19 @@
+// Declaring and Calling Functions
+
+//example 1
+/*
+const sayHello = function () {
+  console.log("Hello, world");
+}
+
+sayHello(); //prints Hello, world
+*/
+
+//example 2
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban"); //prints Hello, Caliban
+sayHello("Miranda"); //prints Hello, Miranda
+sayHello("Ferdinand"); //prints Hello, Ferdinand
