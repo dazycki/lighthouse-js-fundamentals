dd65f31b83198da262edf05f9575b5cfd57f6d27
Daryan Zycki <da.zycki@gmail.com>
2022-01-09 21:44:40 +0000

    Added a function to loop over items in an array.

---
+++ packing-list.js
@@ -0,0 +1,8 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+} //prints all the items in the list 1 by 1
+
+packingList[3]; // prints pans
